0b5b3e09df3da0c9f48093d9033af9f09dc0bbc1
MelancholySeal <kirillazimoff@yandex.ru>
2023-11-27 19:42:23 +0300

    create exm&tasks

--- Project9/gentasks/gentask2.py
+++ Project9/gentasks/gentask2.py
@@ -3,22 +3,17 @@
 
 if __name__ == '__main__':
 
-    # Исходный словарь
     original_dict = {
         1: 'один',
         2: 'два',
         3: 'три',
         4: 'четыре',
-        # Добавьте другие пары ключ-значение
     }
 
-    # Используем метод items() для получения объекта dict_items
     dict_items_object = original_dict.items()
 
-    # Создаем новый словарь "обратный" исходному
     reversed_dict = {value: key for key, value in dict_items_object}
 
-    # Вывод исходного и нового словарей
     print("Исходный словарь:")
     print(original_dict)
 
